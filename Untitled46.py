75e26ede35e039e04d648b6e9e09198d88ff3e9b
nataner <lutcenko.nr@students.dvfu.ru>
2022-05-30 21:11:58 +1000

    natan  1e2

---
+++ Untitled46.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+func = lambda  x, y: x + y
+func(1, 2)
+3
+func('a', 'b')
+'ab'
+(lambda x, y: x + y)(1, 2)
+3
+(lambda x, y: x + y)('a', 'b')
+'ab'
+
+
+# In[ ]:
+
+
+
+
